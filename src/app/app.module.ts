474eed1e2f372fa6929d0b8c9be21ed5bd0ead36
Guilherme C. Tonello <guitonello@outlook.com.br>
2021-04-08 13:24:02 -0300

    Componentes footer e página principal, adicionado rotas e limpeza do componente app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: MainPageComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
